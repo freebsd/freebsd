4d74bd92ea578fc1b35b27d0200ca69864ce176e
Alfonso Gregory <gfunni234@gmail.com>
2021-07-11 14:30:20 -0400

    Remove temporaries

--- sys/libkern/strcspn.c
+++ sys/libkern/strcspn.c
@@ -34,7 +34,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/limits.h>
 
 #define	IDX(c)	((u_char)(c) / LONG_BIT)
-#define	BIT(c)	((u_long)1 << ((u_char)(c) % LONG_BIT))
+#define	BIT(c)	(1UL << ((u_char)(c) % LONG_BIT))
 
 size_t 
 strcspn(const char * __restrict s, const char * __restrict charset) 
@@ -44,30 +44,17 @@ strcspn(const char * __restrict s, const char * __restrict charset)
 	 * generate better code.  Without them, gcc gets a little confused.
 	 */
 	const char *s1;
-	u_long bit;
-	u_long tbl[(UCHAR_MAX + 1) / LONG_BIT];
-	int idx;
 
 	if(*s == '\0')
 		return (0);
 
-#if LONG_BIT == 64	/* always better to unroll on 64-bit architectures */
-	tbl[0] = 1;
-	tbl[3] = tbl[2] = tbl[1] = 0;
-#else
-	for (tbl[0] = idx = 1; idx < sizeof(tbl) / sizeof(tbl[0]); idx++)
-		tbl[idx] = 0;
-#endif
+	u_long tbl[(UCHAR_MAX + 1) / LONG_BIT] = {1}; // Rest of array is zeroes
 	for (; *charset != '\0'; charset++) {
-		idx = IDX(*charset);
-		bit = BIT(*charset);
-		tbl[idx] |= bit;
+		tbl[IDX(*charset)] |= BIT(*charset);
 	}
 
 	for(s1 = s; ; s1++) {
-		idx = IDX(*s1);
-		bit = BIT(*s1);
-		if ((tbl[idx] & bit) != 0)
+		if ((tbl[IDX(*s1)] & BIT(*s1)) != 0)
 			break;
 	}
 	return (s1 - s);
